58d351e229203f04d7b2365ac0d2914ee3a77350
Margulan Baimbet <thecigaroman@gmail.com>
2017-05-25 13:48:16 +0600

    Test commit

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,3 +6,5 @@ mix.options({
 
 mix.sass('public/theme/nanocare/source/scss/app.scss', 'public/theme/nanocare/dist');
 mix.js('public/theme/nanocare/source/js/app.js', 'public/theme/nanocare/dist');
+
+// Random Comment
